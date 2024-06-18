b16ce7db0cce223b9dc56e49f4ee77ec5d7413d1
Daniil Kachur <kachur.daniil@gmail.com>
2024-06-18 23:36:30 +0400

    indentations

--- apps/expo/src/examples/ref.tsx
+++ apps/expo/src/examples/ref.tsx
@@ -8,47 +8,47 @@ import { SafeAreaViewWrapper } from '../components/safe-area-view-wrapper';
 import { DATA } from '../constants/data';
 
 export const Ref = () => {
-    const [currentState, setCurrentState] = useState<State>();
-    const ref = useRef<SelectRef>(null);
+  const [currentState, setCurrentState] = useState<State>();
+  const ref = useRef<SelectRef>(null);
 
-    const onPress = () => {
-        if (ref.current) {
-            ref.current.open();
-        }
-    };
+  const onPress = () => {
+    if (ref.current) {
+      ref.current.open();
+    }
+  };
 
-    const onClear = () => {
-        if (ref.current) {
-            ref.current.clear();
-        }
-    };
+  const onClear = () => {
+    if (ref.current) {
+      ref.current.clear();
+    }
+  };
 
-    const onGetState = () => {
-        if (ref.current) {
-            setCurrentState(ref.current.getState());
-        }
-    };
+  const onGetState = () => {
+    if (ref.current) {
+      setCurrentState(ref.current.getState());
+    }
+  };
 
-    return (
-        <SafeAreaViewWrapper>
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <Button title="Open" onPress={onPress} />
-                <Button title="Reset" onPress={onClear} />
-                <Button title="Get current Select state" onPress={onGetState} />
-                <Select ref={ref} options={DATA} />
-            </View>
+  return (
+    <SafeAreaViewWrapper>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Button title="Open" onPress={onPress} />
+        <Button title="Reset" onPress={onClear} />
+        <Button title="Get current Select state" onPress={onGetState} />
+        <Select ref={ref} options={DATA} />
+      </View>
 
-            <ScrollView style={{ flex: 1 }}>
-                <Text style={{ marginTop: 20 }}>
-                    State: {JSON.stringify(currentState, null, 4)}
-                </Text>
-            </ScrollView>
-        </SafeAreaViewWrapper>
-    );
+      <ScrollView style={{ flex: 1 }}>
+        <Text style={{ marginTop: 20 }}>
+          State: {JSON.stringify(currentState, null, 4)}
+        </Text>
+      </ScrollView>
+    </SafeAreaViewWrapper>
+  );
 };

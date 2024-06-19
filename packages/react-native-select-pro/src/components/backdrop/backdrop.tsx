ee17a7ab2f0a8650b68f6fa61762a6713bfc49b5
Daniil Kachur <kachur.daniil@gmail.com>
2024-06-19 12:10:45 +0400

    indentation

--- packages/react-native-select-pro/src/components/backdrop/backdrop.tsx
+++ packages/react-native-select-pro/src/components/backdrop/backdrop.tsx
@@ -5,30 +5,30 @@ import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
 import type { BackdropProps } from './backdrop.types';
 
 export const Backdrop = <T,>({
-    onOutsidePress,
-    backdropCustomStyles,
-    backdropProps,
-    backdropChildProps,
+  onOutsidePress,
+  backdropCustomStyles,
+  backdropProps,
+  backdropChildProps,
 }: BackdropProps<T>) => {
-    return (
-        <TouchableWithoutFeedback
-            accessibilityLabel="Close a dropdown from outside"
-            accessibilityRole="button"
-            {...backdropProps}
-            onPress={onOutsidePress}
-        >
-            <View {...backdropChildProps} style={[styles.modalOverlay, backdropCustomStyles]} />
-        </TouchableWithoutFeedback>
-    );
+  return (
+    <TouchableWithoutFeedback
+      accessibilityLabel="Close a dropdown from outside"
+      accessibilityRole="button"
+      {...backdropProps}
+      onPress={onOutsidePress}
+    >
+      <View {...backdropChildProps} style={[styles.modalOverlay, backdropCustomStyles]} />
+    </TouchableWithoutFeedback>
+  );
 };
 
 type Styles = {
-    modalOverlay: ViewStyle;
+  modalOverlay: ViewStyle;
 };
 
 const styles = StyleSheet.create<Styles>({
-    modalOverlay: {
-        ...StyleSheet.absoluteFillObject,
-        zIndex: 1,
-    },
+  modalOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1,
+  },
 });
